24c8a0deee451b45ea5779a59209b63db646169e
Abdelrahman Omran <me@omranic.com>
2017-07-30 15:08:42 +0000

    Apply fixes from StyleCI

--- src/Factories/WidgetFactory.php
+++ src/Factories/WidgetFactory.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Rinvex\Widgets\Factories;
 
-use Illuminate\Support\Str;
 use Illuminate\Support\HtmlString;
 use Rinvex\Widgets\Models\AbstractWidget;
 use Rinvex\Widgets\Exceptions\WidgetException;
@@ -19,9 +20,9 @@ class WidgetFactory
     /**
      * Instantiate a widget instance.
      *
-     * @param  string $widget
-     * @param  array  $params
-     * @param  bool   $async
+     * @param string $widget
+     * @param array  $params
+     * @param bool   $async
      *
      * @throws \Rinvex\Widgets\Exceptions\WidgetException
      *
